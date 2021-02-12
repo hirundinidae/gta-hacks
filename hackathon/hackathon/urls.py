bb01f856c5b4aba1ca742376850b7a9cd6a84ee6
Neyha <78521142+ney927@users.noreply.github.com>
2021-02-12 13:22:03 -0500

    Profile migrattions

--- hackathon/hackathon/urls.py
+++ hackathon/hackathon/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
 urlpatterns += [
     path('', include ('mysite.urls')),
+    path('', include("django.contrib.auth.urls")),
 ]
