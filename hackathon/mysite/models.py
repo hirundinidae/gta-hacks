65199c6e966864981200309cb1dda21d9f277d78
Neyha <78521142+ney927@users.noreply.github.com>
2021-02-12 13:07:56 -0500

    User model

--- hackathon/mysite/models.py
+++ hackathon/mysite/models.py
@@ -1,4 +1,21 @@
 from django.db import models
 class submissions(models.Model):
     summary=models.CharField(max_length=2000)
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.CharField(max_length=200)
+    school = models.CharField(max_length=50)
+    prov_choices = [
+        ('QC', 'Quebec'),
+        ('BC', 'British Columbia'),
+        ('ON', 'Ontario'),
+        ('AL', 'Alberta'),
+        ('MB', 'Manitoba'),
+        ('SK', 'Saskatchewan'),
+        ('NL', 'Newfoundland and Labrador'),
+        ('PE', 'Prince Edward Island'),
+        ('NS', 'Nova Scotia'),
+        ('NB', 'New Brunswick'),
+    ]
+    province = models.CharField(max_length=50, choices=prov_choices)
