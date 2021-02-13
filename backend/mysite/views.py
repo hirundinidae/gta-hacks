9837546cfa56ffa2244479fba3f97dd4a25c905a
hirundinidae <diana4lin@gmail.com>
2021-02-13 15:46:42 -0500

    Add get method

--- backend/mysite/views.py
+++ backend/mysite/views.py
@@ -2,14 +2,18 @@ from django.shortcuts import render
 from .models import Profile, Resource
 from .serializers import ProfileSerializer, ResourceSerializer
 from rest_framework import viewsets
+from django.http import JsonResponse
 # from rest_framework import generics
 # from django.views.generic import TemplateView
 
 # Create your views here.
 
 class ProfileView(viewsets.ModelViewSet):
+    viewset = viewsets.ModelViewSet
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
+    def get(self, viewset):
+        return JsonResponse(queryset)
 
 
 class ResourceView(viewsets.ModelViewSet):
