46799dddcfc71082794246ee11143ce5b4413fe3
TRUMPMAN68 <33613826+TRUMPMAN68@users.noreply.github.com>
2021-02-13 15:57:49 -0500

    Added tags

--- backend/mysite/serializers.py
+++ backend/mysite/serializers.py
@@ -11,8 +11,7 @@ class ProfileSerializer(serializers.ModelSerializer):
 class ResourceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Resource
-<<<<<<< Updated upstream
-        fields = ("name", "questions", "answers")
+        fields = ("name", "questions", "answers", "tag_list")
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -27,6 +26,3 @@ class UserSerializer(serializers.ModelSerializer):
         user.set_password(password)
         user.save()
         return user
-=======
-        fields = ("name", "questions", "answers", "tag_list")
->>>>>>> Stashed changes
