1409c6370a175dda1e946f578929a3a75d542416
hirundinidae <diana4lin@gmail.com>
2021-02-12 19:18:06 -0500

    Change to viewsets

--- backend/mysite/urls.py
+++ backend/mysite/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
+from rest_framework import routers
+from django.urls import path, include
 from . import views
 
+router = routers.SimpleRouter()
+router.register(r'profiles', views.ProfileView, 'profiles')
+
 urlpatterns = [
-    path('api/', views.ProfileListCreate.as_view()),
+#     path('api/', views.ProfileListCreate.as_view()),
+    path('api/', include(router.urls))
 ]
