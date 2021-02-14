283800c500c9bd5eee805a9acc8f92947318503e
Prasun Kar <prasunkar24@gmail.com>
2021-02-14 01:40:23 -0500

    Updated result card styling

--- frontend/my-app/components/resultCard.js
+++ frontend/my-app/components/resultCard.js
@@ -1,8 +1,19 @@
-export default function ResultCard({ name }) {
+export default function ResultCard({ name, questions, answers, tags }) {
   return (
-    <div className="result-card w-3/4 h-auto mx-auto p-8 rounded-xl shadow-xl">
-      <h1 className="text-sm">{ name }</h1>
-      <p>test</p>
+    <div className="result-card w-2/5 h-auto mx-6 mb-8 p-8 rounded-xl shadow-lg">
+      <h1>{ name }</h1>
+      <p>{ questions }</p>
+      <p>{ answers }</p>
+      <div className="flex flex-row">
+        {
+          tags && tags.map(tag => {
+            return <span
+                    key={tag.id}
+                    className="py-1 px-4 mt-4 mr-2 border-4 border-blue-600 text-blue-600 font-bold rounded-full"
+                  >{ tag.name }</span>
+          })
+        }
+      </div>
     </div>
   )
 }
